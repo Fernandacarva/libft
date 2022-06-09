33e99c16a37306c8a1cc019c87fdfeceeb75154f
Fernandacarva <fernandacarvalho160.fc@gmail.com>
2022-06-09 15:15:04 -0300

    subindo alteracoes

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ferncarv <ferncarv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 15:55:06 by ferncarv          #+#    #+#             */
-/*   Updated: 2022/05/31 17:56:58 by ferncarv         ###   ########.fr       */
+/*   Updated: 2022/06/09 11:54:05 by ferncarv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const char	*aux1;
+	unsigned char	*aux1;
 
-	aux1 = s;
+	aux1 = (unsigned char *) s;
 	while (n-- > 0)
 	{
-		if (*aux1 == c)
-			return ((char *) aux1);
+		if (*aux1 == (unsigned char) c)
+			return (aux1);
 		aux1++;
 	}
 	return (NULL);
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,37 +6,83 @@
 /*   By: ferncarv <ferncarv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 17:33:30 by ferncarv          #+#    #+#             */
-/*   Updated: 2022/06/01 12:13:58 by ferncarv         ###   ########.fr       */
+/*   Updated: 2022/06/09 14:46:23 by ferncarv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!big || !little)
+		return ((char *)(big));
+	if (!big || little[0] == '\0')
+		return ((char *)(big));
+	i = 0;
+	while (big[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (big[i + j] && little[j]
+			&& i + j < len && big[i + j] == little[j])
+			j++;
+		if (!little[j])
+			return ((char *)(big + i));
+		i++;
+	}
+	return (NULL);
+}*/
+
+
+
+
+
+
 
 #include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	index;
-	int		i_aux2;
+	size_t	i_aux2;
 
 	index = 0;
-	if (*needle == '\0' || needle == NULL)
-		return ((char *)haystack);
+	if (!haystack || !needle || needle[0] == '\0')
+		return ((char *)(haystack));
+
 	while (haystack[index] != '\0' && index < len)
 	{
 		i_aux2 = 0;
 		if (haystack[index] == needle[i_aux2])
 		{
-			while (haystack[index] == needle[i_aux2])
-			{
-				index++;
+			while (haystack[index + i_aux2] == needle[i_aux2] && needle[i_aux2] != '\0')
 				i_aux2++;
-				if (needle[i_aux2] == '\0')
-				{
-					index -= i_aux2;
-					return ((char *)haystack + index);
-				}
-			}
+			if (needle[i_aux2] == '\0')
+				return ((char *)haystack + index);
+			else
+				return (0);
+
 		}
 		index++;
 	}
 	return (0);
 }
+
+/*#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char *a = "Teste";
+	char *b = "est";
+	char *c = "Teste";
+	char *d = "est";
+	size_t s;
+	
+	s = 4;
+	printf("%s\n", ft_strnstr(a, b, s));
+	printf("%s\n", strnstr(c, d, s));
+	return (0);	
+}*/
