07f19a81c61e4b73350fe07bf2c014f2ae6bcb1e
Fernandacarva <fernandacarvalho160.fc@gmail.com>
2022-06-14 10:52:51 -0300

    agora vai

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,31 @@
 /*   By: ferncarv <ferncarv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 17:33:30 by ferncarv          #+#    #+#             */
-/*   Updated: 2022/06/09 15:20:23 by ferncarv         ###   ########.fr       */
+/*   Updated: 2022/06/14 10:51:33 by ferncarv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	index;
-	size_t	i_aux2;
+	size_t	i;
+	size_t	j;
 
-	index = 0;
-	if (!haystack || !needle || needle[0] == '\0')
-		return ((char *)(haystack));
-	while (haystack[index] != '\0' && index < len)
+	if (!big || !little)
+		return ((char *)(big));
+	if (!big || little[0] == '\0')
+		return ((char *)(big));
+	i = 0;
+	while (big[i] != '\0' && i < len)
 	{
-		i_aux2 = 0;
-		if (haystack[index] == needle[i_aux2])
-		{
-			while (haystack[index + i_aux2] == needle[i_aux2]
-				&& needle[i_aux2] != '\0')
-				i_aux2++;
-			if (needle[i_aux2] == '\0')
-				return ((char *)haystack + index);
-			else
-				return (0);
-		}
-		index++;
+		j = 0;
+		while (big[i + j] && little[j]
+			&& i + j < len && big[i + j] == little[j])
+			j++;
+		if (!little[j])
+			return ((char *)(big + i));
+		i++;
 	}
-	return (0);
+	return (NULL);
 }
