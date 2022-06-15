7fb85a1840ade3d9bbf3f963aa0403ff72b38342
Fernandacarva <fernandacarvalho160.fc@gmail.com>
2022-06-15 13:46:36 -0300

    alteracoes2.0

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,27 +6,28 @@
 /*   By: ferncarv <ferncarv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 17:33:30 by ferncarv          #+#    #+#             */
-/*   Updated: 2022/06/14 13:10:12 by ferncarv         ###   ########.fr       */
+/*   Updated: 2022/06/15 13:44:43 by ferncarv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t len)
+char	*ft_strnstr(const char big, const charlittle, size_t len)
 {
-	size_t	s2_size;
-	size_t	i;
+	size_t	sz;
 
-	i = 0;
-	s2_size = ft_strlen(s2);
-	if (!(*s2))
-		return ((char *)s1);
-	while (*s1 && (i < len))
+	sz = ft_strlen(little);
+	if (!sz)
 	{
-		if ((ft_strncmp (s1, s2, s2_size) == 0) && (i + s2_size <= len))
-			return ((char *)s1);
-		s1++;
-		i++;
+		return ((char )big);
 	}
-	return (NULL);
+	while (big && sz <= len--)
+	{
+		if (!ft_strncmp(big, little, sz))
+		{
+			return ((char *)big);
+		}
+		big++;
+	}
+	return (0);
 }
